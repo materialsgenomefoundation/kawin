73cb5c92a22cf4583359da6ff6583c758cf8a338
Ury <nury@ndc.nasa.gov>
2022-02-09 11:27:19 -0800

    updated version number in setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
     url='https://kawin.org/',
-    version='0.0.9',
+    version='0.1.0',
     install_requires=[
         'matplotlib>=3.3',
         'numpy>=1.13',

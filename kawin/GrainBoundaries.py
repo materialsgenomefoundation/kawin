Minor fixes on GBfactors and numerical constraints

kawin/GrainBoundaries.py
@@ -73,7 +73,7 @@ class GBFactors:
         elif self.nucleationSiteType == self.GRAIN_BOUNDARIES:
             self.gbRemoval = np.pi * (1 - self.GBk**2)
             self.areaFactor = 4 * np.pi * (1 - self.GBk)
-            self.volumeFactor = (2 * np.pi / 3) * (2 - 3 * self.GBk + self.GBk**2)
+            self.volumeFactor = (2 * np.pi / 3) * (2 - 3 * self.GBk + self.GBk**3)
 
         #Grain edge nucleation
         elif self.nucleationSiteType == self.GRAIN_EDGES and self.GBk < np.sqrt(3) / 2:

kawin/KWNBase.py
@@ -400,7 +400,9 @@ class PrecipitateBase:
         self.maxVolumeChange = 0.001
         
         self.checkComposition = True
+        self.checkCompositionPre = True
         self.maxCompositionChange = 0.001
+        self.minComposition = 0
 
     def setConstraints(self, **kwargs):
         '''
@@ -927,7 +929,7 @@ class PrecipitateBase:
         index = self.phaseIndex(phase)
         self.interfacialComposition[index] = composition
 
-    def setThermodynamics(self, therm, phase = None):
+    def setThermodynamics(self, therm, phase = None, removeCache = False):
         '''
         Parameters
         ----------
@@ -936,13 +938,13 @@ class PrecipitateBase:
             Phase to consider (defaults to first precipitate in list)
         '''
         index = self.phaseIndex(phase)
-        self.dG[index] = lambda x, T: therm.getDrivingForce(x, T, precPhase=phase)
+        self.dG[index] = lambda x, T, removeCache = removeCache: therm.getDrivingForce(x, T, precPhase=phase, training = removeCache)
         
         if self.numberOfElements == 1:
             self.interfacialComposition[index] = lambda x, T: therm.getInterfacialComposition(x, T, precPhase=phase)
         else:
-            self.interfacialComposition[index] = lambda x, T, dG, R, gExtra: therm.getGrowthAndInterfacialComposition(x, T, dG, R, gExtra, precPhase=phase)
-            self._betaFuncs[index] = lambda x, T: therm.impingementFactor(x, T, precPhase=phase)
+            self.interfacialComposition[index] = lambda x, T, dG, R, gExtra, removeCache = removeCache: therm.getGrowthAndInterfacialComposition(x, T, dG, R, gExtra, precPhase=phase, training = removeCache)
+            self._betaFuncs[index] = lambda x, T, removeCache = removeCache: therm.impingementFactor(x, T, precPhase=phase, training = removeCache)
 
     def setSurrogate(self, surr, phase = None):
         '''
@@ -1112,17 +1114,17 @@ class PrecipitateBase:
             print('N\tTime (s)\tTemperature (K)\tMatrix Comp')
             print('{:.0f}\t{:.1e}\t\t{:.0f}\t\t{:.4f}\n'.format(i, self.time[i], self.T[i], 100*self.xComp[i]))
         else:
-            compStr = 'N\tTime (s)\tTemperature (K)'
-            compValStr = '{:.0f}\t{:.1e}\t\t{:.0f}\t'.format(i, self.time[i], self.T[i])
+            compStr = 'N\tTime (s)\tTemperature (K)\t'
+            compValStr = '{:.0f}\t{:.1e}\t\t{:.0f}\t\t'.format(i, self.time[i], self.T[i])
             for a in range(self.numberOfElements):
                 compStr += self.elements[a] + '\t'
                 compValStr += '{:.4f}\t'.format(100*self.xComp[i,a])
             compValStr += '\n'
             print(compStr)
             print(compValStr)
-        print('\tPhase\tPrec Density (#/m3)\tVolume Frac\tAvg Radius (m)')
+        print('\tPhase\tPrec Density (#/m3)\tVolume Frac\tAvg Radius (m)\tDriving Force (J/mol)')
         for p in range(len(self.phases)):
-            print('\t{}\t{:.3e}\t{:.4f}\t\t{:.4e}'.format(self.phases[p], self.precipitateDensity[p,i], 100*self.betaFrac[p,i], self.avgR[p,i]))
+            print('\t{}\t{:.3e}\t\t{:.4f}\t\t{:.4e}\t{:.4e}'.format(self.phases[p], self.precipitateDensity[p,i], 100*self.betaFrac[p,i], self.avgR[p,i], self.dGs[p,i]*self.VmBeta[p]))
         print('')
                 
     def solve(self, verbose = False, vIt = 1000):
@@ -1242,12 +1244,14 @@ class PrecipitateBase:
             #Calculate nucleation rate
             Z = self._Zeldovich(p, i)
             self.betas[p,i] = self._Beta(p, i)
+            if self.betas[p,i] == 0:
+                return self._noDrivingForce(p, i)
                 
             #Incubation time, either isothermal or nonisothermal
             self.incubationSum[p] = self._incubation(Z, p, i)
             if self.incubationSum[p] > 1:
                 self.incubationSum[p] = 1
-            
+
             return Z * self.betas[p,i] * np.exp(-self.Gcrit[p, i] / (self.kB * self.T[i])) * self.incubationSum[p]
 
         else:
@@ -1281,9 +1285,7 @@ class PrecipitateBase:
         '''
         Set everything to 0 if there is no driving force for precipitation
         '''
-        #self.dGs[p, i] = 0
         self.Rcrit[p, i] = 0
-        #self.incubationOffset[p] = self.time[i-1]
         self.incubationOffset[p] = np.amax([i-1, 0])
         return 0
 

kawin/KWNEuler.py
@@ -516,39 +516,70 @@ class PrecipitateModel (PrecipitateBase):
 
         if i > 1:
             dtPrev = self.time[i-1] - self.time[i-2]
+        else:
+            dtPrev = dt
 
-            #Nucleation rate constraint
-            if self.checkNucleation:
-                dtNuc = dt * np.ones(len(self.phases)+1)
-                for p in range(len(self.phases)):
-                    if self.nucRate[p,i] > self.minNucleationRate and self.nucRate[p,i-1] > self.minNucleationRate and self.nucRate[p,i-1] != self.nucRate[p,i]:
-                        dtNuc[p] = self.maxNucleationRateChange * dtPrev / np.abs(np.log10(self.nucRate[p,i-1] / self.nucRate[p,i]))
-                dt = np.amin(dtNuc)
-                dtAll.append(dt)
+        #Nucleation rate constraint
+        if self.checkNucleation:
+            dtNuc = dt * np.ones(len(self.phases)+1)
+            for p in range(len(self.phases)):
+                if self.nucRate[p,i] > self.minNucleationRate and self.nucRate[p,i-1] > self.minNucleationRate and self.nucRate[p,i-1] != self.nucRate[p,i]:
+                    dtNuc[p] = self.maxNucleationRateChange * dtPrev / np.abs(np.log10(self.nucRate[p,i-1] / self.nucRate[p,i]))
+            dt = np.amin(dtNuc)
+            dtAll.append(dt)
 
-            #Temperature change constraint
-            if self.checkTemperature:
-                Tchange = self.T[i] - self.T[i-1]
-                dtTemp = dt
-                if Tchange > self.maxNonIsothermalDT:
-                    dtTemp = self.maxNonIsothermalDT * (self.time[i] - self.time[i-1]) / Tchange
-                    dt = np.amin([dt, dtTemp])
+        #Temperature change constraint
+        if self.checkTemperature:
+            Tchange = self.T[i] - self.T[i-1]
+            dtTemp = dt
+            if Tchange > self.maxNonIsothermalDT:
+                dtTemp = self.maxNonIsothermalDT * (self.time[i] - self.time[i-1]) / Tchange
+                dt = np.amin([dt, dtTemp])
 
-            if self.checkRcrit:
-                dtRad = dt * np.ones(len(self.phases)+1)
-                if not all((self.Rcrit[:,i-1] == 0) & (self.Rcrit[:,i] - self.Rcrit[:,i-1] == 0) & (self.dGs[:,i] <= 0)):
-                    indices = (self.Rcrit[:,i-1] > 0) & (self.Rcrit[:,i] - self.Rcrit[:,i-1] != 0) & (self.dGs[:,i] > 0)
-                    dtRad[:-1][indices] = self.maxRcritChange * dtPrev / np.abs((self.Rcrit[:,i][indices] - self.Rcrit[:,i-1][indices]) / self.Rcrit[:,i-1][indices])
-                dt = np.amin(dtRad)
-                dtAll.append(dt)
+        if self.checkRcrit:
+            dtRad = dt * np.ones(len(self.phases)+1)
+            if not all((self.Rcrit[:,i-1] == 0) & (self.Rcrit[:,i] - self.Rcrit[:,i-1] == 0) & (self.dGs[:,i] <= 0)):
+                indices = (self.Rcrit[:,i-1] > 0) & (self.Rcrit[:,i] - self.Rcrit[:,i-1] != 0) & (self.dGs[:,i] > 0)
+                dtRad[:-1][indices] = self.maxRcritChange * dtPrev / np.abs((self.Rcrit[:,i][indices] - self.Rcrit[:,i-1][indices]) / self.Rcrit[:,i-1][indices])
+            dt = np.amin(dtRad)
+            dtAll.append(dt)
 
-            if self.checkVolumePre:
-                dtVol = dt * np.ones(len(self.phases) + 1)
-                if not all((self.Rad[:,i]**3*self.nucRate[:,i] > 1e-30)):
-                    indices = (self.Rad[:,i]**3*self.nucRate[:,i] > 1e-30)
-                    dtVol[:-1][indices] = self.maxVolumeChange / (10 * (4*np.pi*self.Rad[:,i][indices]**3*self.nucRate[:,i][indices]/3))
-                dt = np.amin(dtVol)
-                dtAll.append(dt)
+        if self.checkVolumePre or self.checkCompositionPre:
+            dV = np.zeros(len(self.phases))
+            for p in range(len(self.phases)):
+                #Calculate estimate volume change based off growth rate and nucleated particles
+                #TODO: account for non-spherical precipitates
+                dVi = self.PBM[p].PSD * self.PBM[p].PSDsize**2 * 0.5 * (self.growth[p][1:] + self.growth[p][:-1])
+                dVi[dVi < 0] = 0
+                dV = self.VmAlpha / self.VmBeta[p] * (self.GB[p].areaFactor * np.sum(dVi) + self.GB[p].volumeFactor * self.nucRate[p,i] * self.Rad[p,i]**3)
+
+        if self.checkVolumePre:
+            dtVol = dt * np.ones(len(self.phases) + 1)
+            for p in range(len(self.phases)):
+                if dV != 0:
+                    dtVol[p] = self.maxVolumeChange / (2 * np.abs(dV))
+            #if not all((self.Rad[:,i]**3*self.nucRate[:,i] > 1e-30)):
+            #    indices = (self.Rad[:,i]**3*self.nucRate[:,i] > 1e-30)
+            #    dtVol[:-1][indices] = self.maxVolumeChange / (10 * (4*np.pi*self.Rad[:,i][indices]**3*self.nucRate[:,i][indices]/3))
+            dt = np.amin(dtVol)
+            dtAll.append(dt)
+
+        if self.checkCompositionPre:
+            dtComp = dt * np.ones(self.numberOfElements + 1)
+            fvsum = np.sum(self.betaFrac[:,i-1])
+            xbavg = np.zeros(self.numberOfElements)
+            if self.numberOfElements == 1:
+                xbavg[0] = 0 if fvsum == 0 else (self.xComp[0] - self.xComp[i-1] * (1 - fvsum)) / fvsum
+                dxadt = (self.xComp[i-1] - xbavg) * np.sum(dV) / (1 - fvsum)
+            else:
+                for e in range(self.numberOfElements):
+                    xbavg[e] = 0 if fvsum == 0 else (self.xComp[0,e] - self.xComp[i-1,e] * (1 - fvsum)) / fvsum
+                dxadt = (self.xComp[i-1,:] - xbavg) * np.sum(dV) / (1 - fvsum)
+            dxadt[dxadt == 0] = self.maxCompositionChange / (2 * dt)
+            dtComp[:self.numberOfElements] = self.maxCompositionChange / (2 * dxadt)
+                
+            dt = np.amin(dtComp)
+            dtAll.append(dt)
 
         #Minimum dt is the lower of the minimum allowed time increment or the time to the next pre-defined increment
         minDT = self._calculateDT(i-1, self.minDTFraction)
@@ -593,9 +624,9 @@ class PrecipitateModel (PrecipitateBase):
 
         if self.checkComposition:
             if self.numberOfElements == 1:
-                compCheck = np.abs(self.xComp[i] - self.xComp[i-1]) < self.maxCompositionChange
+                compCheck = (np.abs(self.xComp[i] - self.xComp[i-1]) < self.maxCompositionChange) & (self.xComp[i] > 0)
             else:
-                compCheck = np.amax(np.abs(self.xComp[i,:] - self.xComp[i-1,:])) < self.maxCompositionChange
+                compCheck = (np.amax(np.abs(self.xComp[i,:] - self.xComp[i-1,:])) < self.maxCompositionChange) & (np.amin(self.xComp[i,:] > self.minComposition))
         else:
             compCheck = True
 
@@ -764,6 +795,7 @@ class PrecipitateModel (PrecipitateBase):
         else:
             if np.sum(fBeta) < 1:
                 self.xComp[i] = (self.xComp[0] - np.sum(fConc, axis=0)) / (1 - np.sum(fBeta))
+                self.xComp[i][self.xComp[i] < 0] = self.minComposition
             else:
                 self.xComp[i] = np.zeros(self.numberOfElements)
 
@@ -854,7 +886,6 @@ class PrecipitateModel (PrecipitateBase):
         growthRate = []
         for p in range(len(self.phases)):
             growthRate.append(self._singleGrowthMulti(i, p))
-
         self.growth = growthRate
 
     def plot(self, axes, variable, bounds = None, timeUnits = 's', radius='spherical', *args, **kwargs):

kawin/Thermodynamics.py
@@ -98,7 +98,6 @@ class GeneralThermodynamics:
         
         #Pertains to parent phase (composition, sampled points, equilibrium calculations)
         self._prevX = None
-        self._pointsParent = None
         self._parentEq = None
 
         #Pertains to precipitate phases (sampled points)
@@ -598,6 +597,9 @@ class GeneralThermodynamics:
             #If not, then there's no composition set to cache
             if len(precip_idx) > 0:
                 cs_matrix = CompositionSet(self.phase_records[self.phases[0]])
+                #If there's a miscibility gap in the matrix phase, then take the largest value
+                if len(matrix_idx) > 1:
+                    matrix_idx = [np.argmax(phase_amounts[matrix_idx])]
                 cs_matrix.update(eq.Y.isel(vertex=matrix_idx).values.ravel()[:cs_matrix.phase_record.phase_dof],
                                 phase_amounts[matrix_idx], state_variables)
                 cs_precip = CompositionSet(self.phase_records[precPhase])
@@ -1167,12 +1169,12 @@ class MulticomponentThermodynamics (GeneralThermodynamics):
         #Near saturation, pycalphad may detect only a single phase (if sampling density is too low)
         #When this occurs, this will assume that the system is on the same tie-line and 
         #use the previously calculated values
-        self._prevDc = None
-        self._prevMc = None
-        self._prevGba = None
-        self._prevBeta = None
-        self._prevCa = None
-        self._prevCb = None
+        self._prevDc = {p: None for p in phases[1:]}
+        self._prevMc = {p: None for p in phases[1:]}
+        self._prevGba = {p: None for p in phases[1:]}
+        self._prevBeta = {p: None for p in phases[1:]}
+        self._prevCa = {p: None for p in phases[1:]}
+        self._prevCb = {p: None for p in phases[1:]}
 
     def getInterfacialComposition(self, x, T, gExtra = 0, precPhase = None):
         '''
@@ -1296,16 +1298,16 @@ class MulticomponentThermodynamics (GeneralThermodynamics):
 
         Will return (None, None, None, None, None, None) if single phase
         '''
+        if precPhase is None:
+            precPhase = self.phases[1]
+
         #Check if input equilibrium has converged
         if np.any(np.isnan(chemical_potentials)):
             if training:
                 return None, None, None, None, None, None
             else:
                 print('Warning: equilibrum was not able to be solved for, using results of previous calculation')
-                return self._prevDc, self._prevMc, self._prevGba, self._prevBeta, self._prevCa, self._prevCb
-
-        if precPhase is None:
-            precPhase = self.phases[1]
+                return self._prevDc[precPhase], self._prevMc[precPhase], self._prevGba[precPhase], self._prevBeta[precPhase], self._prevCa[precPhase], self._prevCb[precPhase]
 
         ele = list(composition_sets[0].phase_record.nonvacant_elements)
         refIndex = ele.index(self.elements[0])
@@ -1355,16 +1357,20 @@ class MulticomponentThermodynamics (GeneralThermodynamics):
 
             betaNum = xBarFull**2
             betaDen = Dtrace * xMFull.flatten()
-            beta = 1 / np.sum(betaNum / betaDen)
+            bsum = np.sum(betaNum / betaDen)
+            if bsum == 0:
+                beta = self._prevBeta[precPhase]
+            else:
+                beta = 1 / bsum
 
-            self._prevDc = num[unsortIndices] / den
-            self._prevMc = 1 / den
-            self._prevGba = Gba
-            self._prevBeta = beta
-            self._prevCa = xM[unsortIndices]
-            self._prevCb = xP[unsortIndices]
+            self._prevDc[precPhase] = num[unsortIndices] / den
+            self._prevMc[precPhase] = 1 / den
+            self._prevGba[precPhase] = Gba
+            self._prevBeta[precPhase] = beta
+            self._prevCa[precPhase] = xM[unsortIndices]
+            self._prevCb[precPhase] = xP[unsortIndices]
 
-            return self._prevDc, self._prevMc, self._prevGba, self._prevBeta, self._prevCa, self._prevCb
+            return self._prevDc[precPhase], self._prevMc[precPhase], self._prevGba[precPhase], self._prevBeta[precPhase], self._prevCa[precPhase], self._prevCb[precPhase]
         else:
             if training:
                 return None, None, None, None, None, None
@@ -1374,7 +1380,7 @@ class MulticomponentThermodynamics (GeneralThermodynamics):
 
                 #If two-phase equilibrium is not found, then the temperature may have changed to where the precipitate is unstable
                 #Return None in this case
-                return None, None, None, self._prevBeta, None, None
+                return None, None, None, self._prevBeta[precPhase], None, None
 
 
     def curvatureFactor(self, x, T, precPhase = None, training = False):
@@ -1433,6 +1439,9 @@ class MulticomponentThermodynamics (GeneralThermodynamics):
                     return self._prevDc, self._prevMc, self._prevGba, self._prevBeta, self._prevCa, self._prevCb
 
             cs_matrix = CompositionSet(self.phase_records[self.phases[0]])
+            #If there's a miscibility gap in the matrix phase, then take the largest value
+            if len(matrix_idx) > 1:
+                matrix_idx = [np.argmax(phase_amounts[matrix_idx])]
             cs_matrix.update(eq.Y.isel(vertex=matrix_idx).values.ravel()[:cs_matrix.phase_record.phase_dof],
                              phase_amounts[matrix_idx], state_variables)
 
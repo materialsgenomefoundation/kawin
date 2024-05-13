d60346b629f815678895d38c8bf8f2436c8d0ff9
Ury <ury3@llnl.gov>
2024-05-13 10:41:35 -0700

    additional comments

--- kawin/mobility_fitting/liquid_mobility.py
+++ kawin/mobility_fitting/liquid_mobility.py
@@ -46,6 +46,22 @@ def compute_liquid_mobility_liu(database, species):
     Units:
         diameter: Angstroms
         T_m: K
+
+    From Y. Liu et al, "A predictive equation for solute diffusivity in liquid metals" Scripta Materialia 55 (2006), 367
+
+    Q = 0.17*R*Tm*(16+K0) J/mol
+        K0 is integer corresponding to solid phase at melting (BCC = 1, HCP = 2, FCC = 3)
+        If the phase is neither of those 3, then we assume K0 = 0
+
+    For self diffusion
+        D0_BB = (8.95 - 0.0734*A) * 1e-8 m2/s
+
+    For solute diffusion
+        D0_AB = r_B / r_A * D0_BB
+
+    Then M_AB = D0_AB * exp(-Q/RT)
+    
+    In a calphad database, this becomes MQ = -Q + R*T*ln(D0_AB)
     '''
     R = 8.314
     
@@ -82,6 +98,34 @@ def compute_liquid_mobility_su(database, species):
         density: g/cm3
         T_m: K
         CTE: m/m
+
+    From X. Su et al, "A new equation for temperature dependent solute impurity diffusivity in liquid metals" Journal of Phase Equilibria and Diffusion 31 (2010) 333
+
+    Diffusivity starts with Sutherlan-Einstein equation - D = k*T / 4*pi*mu*r
+
+    Dynamic viscosity (mu)
+        mu_B = C1 * M_B^1/2 * T^1/2 / V_B^2/3 * exp(C2 * TM_B / T) J*s/m3
+        C1 = 1.8e-8 (J/K/mol^1/3)^1/2
+        C2 = 2.34
+
+    Radius in liquid
+        r_A = R0 * (M_A/rho_A)^1/3 * (1 - 0.122 * (T/TM)^1/2) m
+            R0 = 0.644e-10
+        Note: In Su et al, TM is suggested to be melting point of solute (TM_A), but in
+        P. Protopapas et al, J. Chem. Phys. 59 (1973) 15 (where this equation comes from), 
+        TM is noted to be the melting point of the alloy, in which the solvent (TM_B) is used
+        Here, we used TM_B for the melting point since it seems to be the trend of smaller radius
+        leading to higher diffusivity
+
+    D_AB = k*T / 4*pi*mu_B*r_A
+
+    To put in a Calphad database, it's helpful to define equivalents for D0_AB and Q
+        Q = -C2 * TM_B * R (coming from the viscosity equation)
+        D0_AB = D0 * T^1/2 / (1 - 0.112 * (T/TM_B)^1/2)
+            D0 = (k / 4*pi) * (V_B^2/3 / C1*M_B^1/2) * (rho_A^1/3 / M_A^1/3*R0)
+        Then M = D_AB = D0_AB * exp(-Q/RT)
+        And MQ = -Q + R*T*ln(D0_AB)
+    
     '''
     vIndex = find_last_variable(database)
     C1 = 1.8e-8

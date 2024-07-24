d030474d5bfb709d5cfe0f9d0dfeeccc7422c811
Nicholas Ury <ury3@llnl.gov>
2024-07-24 16:25:09 -0700

    change python support to 3.9-3.12

--- setup.py
+++ setup.py
@@ -41,10 +41,10 @@ setup(
 
         # Supported Python versions
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 
 )

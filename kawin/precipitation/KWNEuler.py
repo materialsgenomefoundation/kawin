368655bf20e02af038193f58be28dbbaeeba5aa3
nury12n <nury12n@gmail.com>
2025-04-26 14:33:51 -0700

    FIX: dislocation and bulk nucleation site calculations are distinguished properly

--- kawin/precipitation/KWNEuler.py
+++ kawin/precipitation/KWNEuler.py
@@ -386,13 +386,14 @@ class PrecipitateModel (PrecipitateBase):
         #If parent phases exists, then calculate the number of potential nucleation sites on the parent phase
         # #This is the number of lattice sites on the total surface area of the parent precipitate
         nucleationSites = np.sum([4*np.pi*self.PBM[p2].SecondMomentFromN(x[p2]) * (AVOGADROS_NUMBER/VmBetas[p2])**(2/3) for p2 in parentPhases[p]])
-        if isinstance(nucParams[p].description, BulkDescription):
-            bulkPrec = np.sum([self.PBM[p2].ZeroMomentFromN(x[p2]) for p2 in range(len(self.phases)) if isinstance(nucParams[p2].description, BulkDescription)])
-            nucleationSites += self.matrixParameters.nucleationSites.bulkN0 - bulkPrec
-
-        elif isinstance(nucParams[p].description, DislocationDescription):
+        # DislocationDescription is a subclass of BulkDescription, so we test this first
+        if isinstance(nucParams[p].description, DislocationDescription):
             bulkPrec = np.sum([self.PBM[p2].FirstMomentFromN(x[p2]) for p2 in range(len(self.phases)) if isinstance(nucParams[p2].description, DislocationDescription)])
             nucleationSites += self.matrixParameters.nucleationSites.dislocationN0 - bulkPrec * (AVOGADROS_NUMBER / self.matrixParameters.volume.Vm)**(1/3)
+
+        elif isinstance(nucParams[p].description, BulkDescription):
+            bulkPrec = np.sum([self.PBM[p2].ZeroMomentFromN(x[p2]) for p2 in range(len(self.phases)) if isinstance(nucParams[p2].description, BulkDescription)])
+            nucleationSites += self.matrixParameters.nucleationSites.bulkN0 - bulkPrec
 
         elif isinstance(nucParams[p].description, GrainBoundaryDescription):
             boundPrec = np.sum([nucParams[p2].gbRemoval * self.PBM[p2].SecondMomentFromN(x[p2]) for p2 in range(len(self.phases)) if isinstance(nucParams[p2].description, GrainBoundaryDescription)])
